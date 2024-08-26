d1ddc7c30eb8e068b1d63681054599b0ffe390b0
Ryan-Hozy <ryanho.rhzy@gmail.com>
2024-08-27 06:14:39 +0800

    Initial commit

---
+++ types/image.d.ts
@@ -0,0 +1,27 @@
+//The code you provided is a set of TypeScript module declarations for importing image files in a TypeScript project. These declarations allow TypeScript 
+//to understand how to handle imports of image files like .png, .jpg, .jpeg, .gif, and .svg.
+
+declare module "*.png" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.gif" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.svg" {
+    const value: any;
+    export default value;
+}
