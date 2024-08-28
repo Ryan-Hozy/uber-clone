52ab1907589ea78c0b4b506dfdab3ceecaf9f94e
Ryan-Hozy <ryanho.rhzy@gmail.com>
2024-08-28 11:26:57 +0800

    Modified the global layout in the (root) folder and added

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,10 +1,19 @@
+import { icons } from '@/app/constants';
 import { Tabs } from 'expo-router';
-import { View, Image } from 'react-native';
+import { View, Image, ImageSourcePropType } from 'react-native';
 
-const TabIcon = () => (
-  <View>
-    <View>
-      <Image />
+const TabIcon = ({ source, focused} : {source: ImageSourcePropType, focused: Boolean}) => (
+  <View className={`flex flex-row justify-center 
+  items-center rounded-full ${focused ? "bg-general-300" : ""}`}>
+    <View className={`rounded-full w-12 h-12 items-center 
+    justify-center ${focused ? "bg-general-400" : ""}`}>
+      <Image 
+        source={source} 
+        tintColor="white" 
+        resizeMode='contain'
+        className='w-7 h-7'
+      
+      />
     </View>
   </View>
 )
@@ -12,17 +21,58 @@ const TabIcon = () => (
 const Layout = () =>  (
       <Tabs initialRouteName='index' screenOptions={{
         tabBarActiveTintColor:"white",
+        tabBarInactiveTintColor:"white",
+        tabBarShowLabel:false,
+        tabBarStyle: {
+          backgroundColor: "#333333",
+          borderRadius: 50,
+          paddingBottom: 0,
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          //TODO: all these below need to confirm how it would affect the tab bar when there is content
+          display: "flex",
+          justifyContent:"space-between",
+          alignItems:"center",
+          flexDirection:"row",
+          position:"absolute"
+        }
       }}>
         <Tabs.Screen
           name="home"
           options={{
             title: 'Home',
             headerShown: false,
-            tabBarIcon: () => <TabIcon/>
+            tabBarIcon: ({focused}) => <TabIcon focused={focused} source={icons.home}/>
+          }}
+        />
+          <Tabs.Screen
+          name="rides"
+          options={{
+            title: 'Rides',
+            headerShown: false,
+            tabBarIcon: ({focused}) => <TabIcon focused={focused} source={icons.list}/>
+          }}
+        />
+          <Tabs.Screen
+          name="chat"
+          options={{
+            title: 'Chat',
+            headerShown: false,
+            tabBarIcon: ({focused}) => <TabIcon focused={focused} source={icons.chat}/>
+          }}
+        />
+          <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({focused}) => <TabIcon focused={focused} source={icons.profile}/>
           }}
         />
 
-      </Tabs>>
+      </Tabs>
     
   );
 
