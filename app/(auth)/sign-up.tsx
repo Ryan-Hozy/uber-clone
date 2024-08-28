16df998e644c2c6788c05fc49727a61601575f3c
Ryan-Hozy <ryanho.rhzy@gmail.com>
2024-08-28 21:18:02 +0800

    created a user database with neon console. If a user sign up the info such as name, email, and Clerk Id will be assigned to neon console

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,6 +7,7 @@ import CustomButton from "@/components/CustomButton";
 import OAuth from "@/components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
 import { ReactNativeModal } from "react-native-modal";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
     const { isLoaded, signUp, setActive} = useSignUp()
@@ -50,7 +51,16 @@ const SignUp = () => {
     
           if (completeSignUp.status === 'complete') {
             //Create a database User
-            
+            await fetchAPI('/(api)/user', {
+                method: "POST",
+                body: JSON.stringify({
+                   name: form.name,
+                   email: form.email,
+                   clerkId: completeSignUp.createdUserId,
+                })
+            })
+
+
             await setActive({ session: completeSignUp.createdSessionId })
             setVerification({...verification, state: "success"})
           } else {
